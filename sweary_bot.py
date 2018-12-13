91f580b30a96b49570fa62fdfbb7149c2e6be3c7
Erysiphales <Samuel.Holden@sainsbury-laboratory.ac.uk>
2018-12-13 17:18:15 +0000

    Update sweary_bot.py
    Adds one response, fixes typo

--- sweary_bot.py
+++ sweary_bot.py
@@ -3,7 +3,7 @@ def sweary_bot():
 # and bask in the sweary incredulity of sweary_bot!
     input('What are we angry about now? \n')
     from random import randint
-    a = randint(0,3) #selects a random integer between 0 and 9. Increase this if you wat to add more responses
+    a = randint(0,4) #selects a random integer between 0 and 4. Increase this if you want to add more responses
     if a==0: #the integer selected corresponds to the responses below. Customize as you like.
         print ("ARE YOU FUCKING KIDDING ME?! \n")
     elif a==1:
@@ -12,3 +12,5 @@ def sweary_bot():
         print ("ASFJHFDJGKJLREHGWFFFFUUUUUUUUUCK! \n")
     elif a==3:
         print ("FUCK! \n")
+    elif a==4:
+        print ("BY LUCIFER'S BALLS!!1!!1 \n")
